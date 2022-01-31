1fc3f0a9ab2662f82108941f9f5c998448a86450
jayasting98 <65202977+jayasting98@users.noreply.github.com>
2022-01-31 15:23:27 +0800

    Remove placeholder data

--- src/web/app/pages-admin/admin-responses-page/admin-responses-page.component.ts
+++ src/web/app/pages-admin/admin-responses-page/admin-responses-page.component.ts
@@ -31,150 +31,8 @@ export class AdminResponsesPageComponent implements OnInit {
   dateToday: DateFormat = { year: 0, month: 0, day: 0 };
   earliestSearchDate: DateFormat = { year: 0, month: 0, day: 0 };
   queryParams: { startTime: number, endTime: number } = { startTime: 0, endTime: 0 };
-
-  // TODO: This is a placeholder.
-  data: { value: number, date: string }[] = [
-    {
-      value: 20,
-      date: '2020-05-12T12:19:00+00:00',
-    },
-    {
-      value: 50,
-      date: '2020-05-14T12:19:00+00:00',
-    },
-    {
-      value: 30,
-      date: '2020-05-16T12:19:00+00:00',
-    },
-    {
-      value: 80,
-      date: '2020-05-18T12:19:00+00:00',
-    },
-    {
-      value: 55,
-      date: '2020-05-20T12:19:00+00:00',
-    },
-    {
-      value: 60,
-      date: '2020-05-22T12:19:00+00:00',
-    },
-    {
-      value: 45,
-      date: '2020-05-24T12:19:00+00:00',
-    },
-    {
-      value: 30,
-      date: '2020-05-26T12:19:00+00:00',
-    },
-    {
-      value: 40,
-      date: '2020-05-28T12:19:00+00:00',
-    },
-    {
-      value: 70,
-      date: '2020-05-30T12:19:00+00:00',
-    },
-    {
-      value: 63,
-      date: '2020-06-01T12:19:00+00:00',
-    },
-    {
-      value: 40,
-      date: '2020-06-03T12:19:00+00:00',
-    },
-    {
-      value: 50,
-      date: '2020-06-05T12:19:00+00:00',
-    },
-    {
-      value: 75,
-      date: '2020-06-07T12:19:00+00:00',
-    },
-    {
-      value: 20,
-      date: '2020-06-09T12:19:00+00:00',
-    },
-    {
-      value: 50,
-      date: '2020-06-11T12:19:00+00:00',
-    },
-    {
-      value: 80,
-      date: '2020-06-13T12:19:00+00:00',
-    },
-    {
-      value: 75,
-      date: '2020-06-15T12:19:00+00:00',
-    },
-    {
-      value: 82,
-      date: '2020-06-17T12:19:00+00:00',
-    },
-    {
-      value: 55,
-      date: '2020-06-19T12:19:00+00:00',
-    },
-    {
-      value: 35,
-      date: '2020-06-21T12:19:00+00:00',
-    },
-    {
-      value: 34,
-      date: '2020-06-23T12:19:00+00:00',
-    },
-    {
-      value: 45,
-      date: '2020-06-25T12:19:00+00:00',
-    },
-    {
-      value: 58,
-      date: '2020-06-27T12:19:00+00:00',
-    },
-    {
-      value: 34,
-      date: '2020-06-29T12:19:00+00:00',
-    },
-    {
-      value: 60,
-      date: '2020-07-01T12:19:00+00:00',
-    },
-    {
-      value: 75,
-      date: '2020-07-03T12:19:00+00:00',
-    },
-    {
-      value: 80,
-      date: '2020-07-05T12:19:00+00:00',
-    },
-    {
-      value: 29,
-      date: '2020-07-07T12:19:00+00:00',
-    },
-    {
-      value: 40,
-      date: '2020-07-09T12:19:00+00:00',
-    },
-    {
-      value: 54,
-      date: '2020-07-11T12:19:00+00:00',
-    },
-    {
-      value: 67,
-      date: '2020-07-13T12:19:00+00:00',
-    },
-    {
-      value: 90,
-      date: '2020-07-15T12:19:00+00:00',
-    },
-    {
-      value: 84,
-      date: '2020-07-17T12:19:00+00:00',
-    },
-    {
-      value: 43,
-      date: '2020-07-19T12:19:00+00:00',
-    },
-  ];
+  hasQueried: boolean = false;
+  data: { value: number, date: string }[] = [];
 
   constructor(
     private feedbackResponseStatisticsService: FeedbackResponseStatisticsService,
@@ -219,6 +77,7 @@ export class AdminResponsesPageComponent implements OnInit {
           date: new Date(feedbackResponseStatistic.timestamp).toISOString(),
         });
       }
+      this.hasQueried = true;
     });
   }
 
